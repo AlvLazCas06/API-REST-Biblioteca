d59b355ff06bc3db2d801a80a06ddf66a5e1b9c1
Alvaro Lázaro Castellón <alvarolazaro101@gmail.com>
2025-11-20 14:28:51 +0100

    Adición de la anotación ResponseStatus para el manejo de errores

--- Biblioteca/src/main/java/com/salesianostriana/dam/biblioteca/errors/GlobalExceptionHandler.java
+++ Biblioteca/src/main/java/com/salesianostriana/dam/biblioteca/errors/GlobalExceptionHandler.java
@@ -5,6 +5,7 @@ import com.salesianostriana.dam.biblioteca.exception.InvalidBibliotecaDataExcept
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ProblemDetail;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
@@ -13,6 +14,7 @@ import java.net.URI;
 @RestControllerAdvice
 public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
+    @ResponseStatus(HttpStatus.NOT_FOUND)
     @ExceptionHandler(BibliotecaNotFoundException.class)
     public ProblemDetail handleBibliotecaNotFoundException(BibliotecaNotFoundException ex) {
         ProblemDetail result = ProblemDetail.forStatusAndDetail(HttpStatus.NOT_FOUND, ex.getMessage());
